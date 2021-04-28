79bde5d2b2e35279044632ff7dd97deb626456de
Luca Cracco <craccoluca@gmail.com>
2021-04-28 19:26:22 +0200

    Integrated task for Twig

---
+++ src/Task/Twig/Tasks.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace LucaCracco\RoboDrupal\Task\Twig;
+
+/**
+ * Trait Tasks for Twig.
+ *
+ * @package LucaCracco\RoboDrupal\Task\Twig
+ */
+trait Tasks {
+
+  /**
+   * Task twig.
+   *
+   * @return \LucaCracco\RoboDrupal\Task\Twig\TwigTask
+   *   Drush task.
+   */
+  protected function taskTwig() {
+
+    /** @var \LucaCracco\RoboDrupal\Task\Twig\TwigTask $task */
+    $task = $this->task(TwigTask::class);
+
+    /** @var \Symfony\Component\Console\Output\OutputInterface $output */
+    $output = $this->output();
+    $task->setVerbosityThreshold($output->getVerbosity());
+
+    return $task;
+  }
+
+}
