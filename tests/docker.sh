c88a25e451b7382cc5ed6b60bdf6509ea75d0eca
Luca Cracco <craccoluca@gmail.com>
2023-02-26 18:02:36 +0100

    Remove grasmash/yaml-expander, update docker image for tests, updated tpl settings/services, remove ConfigLoader trait

--- tests/docker.sh
+++ tests/docker.sh
@@ -6,8 +6,8 @@ set -e
 PHP_VERSION=${PHP_VERSION:='7.4'}
 DRUPAL_CORE_CONSTRAINT="${DRUPAL_CORE_CONSTRAINT:=~9.3.0}"
 
-echo -e "Run tests with PHP $PHP_VERSION for Drupal $DRUPAL_CORE_CONSTRAINT\n"
+echo -e "\n\e[94m[info] Run tests with PHP $PHP_VERSION for Drupal $DRUPAL_CORE_CONSTRAINT \e[0m\n"
 docker run -it --rm -v "$PWD":/var/www/html/robo-drupal -w /var/www/html/robo-drupal \
   --env DRUPAL_CORE_CONSTRAINT=$DRUPAL_CORE_CONSTRAINT \
-  php:$PHP_VERSION \
+  drupal:php${PHP_VERSION}-fpm-buster \
   /bin/bash -c "./tests/setup.sh && ./tests/test.sh"
--- tests/test.sh
+++ tests/test.sh
@@ -8,82 +8,84 @@ if ! command -v composer &>/dev/null; then
   exit
 fi
 
-echo -e "\n\nDirectory running ${PWD}\n"
+echo -e "\n\e[94m[info] Directory running ${PWD} \e[0m\n"
 FOLDER_REPO=${FOLDER_REPO:="${PWD}"}
 FOLDER_TESTS=${FOLDER_TESTS:="${PWD}/../robo-drupal-demo"}
-DRUPAL_CORE_CONSTRAINT="${DRUPAL_CORE_CONSTRAINT:=~9.3.0}"
+DRUPAL_CORE_CONSTRAINT="${DRUPAL_CORE_CONSTRAINT:=~9.4.0}"
 
-echo -e "\nClear/Create directory of install drupal for tests\n"
+echo -e "\n\e[94m[info] Clear/Create directory of install drupal for tests \e[0m\n"
 rm -rf $FOLDER_TESTS
 mkdir -p $FOLDER_TESTS
 chmod 775 -R $FOLDER_TESTS
 rm -Rf $FOLDER_TESTS
 
-echo -e "\nInstalling Drupal $DRUPAL_CORE_CONSTRAINT on $FOLDER_TESTS\n"
+echo -e "\n\e[94m[info] Installing Drupal $DRUPAL_CORE_CONSTRAINT on $FOLDER_TESTS \e[0m\n"
 composer create-project drupal/recommended-project:$DRUPAL_CORE_CONSTRAINT $FOLDER_TESTS --no-install
 composer config allow-plugins true --working-dir=$FOLDER_TESTS
 composer install --prefer-dist --no-interaction --working-dir=$FOLDER_TESTS
 
-echo -e "\nInstalling Requirements\n"
+echo -e "\n\e[94m[info] Installing Requirements \e[0m\n"
 composer require --no-interaction --working-dir=$FOLDER_TESTS \
   drupal/core-recommended:$DRUPAL_CORE_CONSTRAINT \
   drupal/core-dev:$DRUPAL_CORE_CONSTRAINT \
   drupal/core-composer-scaffold:$DRUPAL_CORE_CONSTRAINT \
   drupal/core:$DRUPAL_CORE_CONSTRAINT
 
-echo -e "\nInstalling RoboDrupal: set custom repository for RoboDrupal\n"
+echo -e "\n\e[94m[info] Installing RoboDrupal: set custom repository for RoboDrupal \e[0m\n"
 composer config --working-dir=$FOLDER_TESTS repositories.1 "{\"type\": \"path\", \"url\": \"$FOLDER_REPO\", \"symlink\": true}"
 composer require --no-interaction --working-dir=$FOLDER_TESTS lucacracco/robo-drupal:@dev
 
-echo -e "\nCopy template settings\n"
+echo -e "\n\e[94m[info] Copy template settings \e[0m\n"
 cp -v "$FOLDER_REPO/tests/template/tpl.settings.php" "$FOLDER_TESTS/web/sites/default/tpl.settings.php"
 cp -v "$FOLDER_REPO/tests/template/tpl.services.yml" "$FOLDER_TESTS/web/sites/default/tpl.services.yml"
 cp -v "$FOLDER_REPO/tests/template/robo.yml" "$FOLDER_TESTS/robo.yml"
 cp -v "$FOLDER_REPO/tests/template/RoboFile.php" "$FOLDER_TESTS/RoboFile.php"
 
-echo -e "\nOpen folder test: $FOLDER_TESTS\n"
+echo -e "\n\e[94m[info] Open folder test: $FOLDER_TESTS \e[0m\n"
 cd "$FOLDER_TESTS"
 
 # Debug
-#echo -e "\nCheck symlink from $FOLDER_TESTS to $FOLDER_REPO\n"
-#echo -e "\nCurrent path $FOLDER_TESTS\n"
+#echo -e "\n\e[94m[info] Check symlink from $FOLDER_TESTS to $FOLDER_REPO \e[0m\n"
+#echo -e "\n\e[94m[info] Current path $FOLDER_TESTS \e[0m\n"
 #ls -alt "$FOLDER_TESTS/vendor/lucacracco"
 #ls -alt "$FOLDER_REPO"
 
 # Print versioning.
-echo -e "\nPrint versioning Tools\n"
+echo -e "\n\e[94m[info] Print versioning Tools \e[0m\n"
+php -v
+composer --version
 ./vendor/bin/robo --version
 ./vendor/bin/drush --version
+./vendor/bin/drush status
 
-echo -e "\nUse template settings from twig\n"
-cp -v "$FOLDER_REPO/tests/template/tpl.settings.php.twig" "$FOLDER_TESTS/web/sites/default/tpl.settings.php.twig"
+echo -e "\n\e[94m[info] Use template settings \e[0m\n"
 ./vendor/bin/robo scaffold
 
-echo -e "\nInstall Drupal: Minimal profile\n"
+echo -e "\n\e[94m[info] Install Drupal: Minimal profile \e[0m\n"
 ./vendor/bin/robo install minimal
 
-echo -e "\nExport configurations\n"
+echo -e "\n\e[94m[info] Export configurations \e[0m\n"
 ./vendor/bin/robo config:export
 
-echo -e "\nRebuild cache\n"
+echo -e "\n\e[94m[info] Rebuild cache \e[0m\n"
 ./vendor/bin/robo cache-rebuild
 
-echo -e "\nInstall Drupal from configurations\n"
+echo -e "\n\e[94m[info] Install Drupal from configurations \e[0m\n"
 ./vendor/bin/robo install:config minimal
 
-echo -e "\nUpdate configuration\n"
+echo -e "\n\e[94m[info] Update configuration \e[0m\n"
 ./vendor/bin/drush config-set --no-interaction system.site name "Custom name"
 
-echo -e "\nDeploy\n"
+echo -e "\n\e[94m[info] Deploy \e[0m\n"
 ./vendor/bin/robo deploy
 
-echo -e "\nExport database in gzip\n"
+echo -e "\n\e[94m[info] Export database in gzip \e[0m\n"
 ./vendor/bin/robo database:export /tmp --gzip
 # todo: check file exist.
 ls /tmp -alt
 
-echo -e "\nExport database with custom filename\n"
+echo -e "\n\e[94m[info] Export database with custom filename \e[0m\n"
 ./vendor/bin/robo database:export /tmp --filename=drupal-dump
 
-#echo -e "\nInstall Drupal from database\n"
+#echo -e "\n\e[94m[info] Install Drupal from database \e[0m\n"
 #./vendor/bin/robo install:database /tmp/drupal-dump.sql
